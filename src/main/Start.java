caaee87e42c55ddc587ca3d0a6026bbc54dfb6f4
Dieguin <Dieguin@DESKTOP-IGAS87S>
2024-04-25 20:25:32 +0200

    SEXTO COMMIT
    -Clase main start creada para comprobar que las personas validas se
    imprime.

--- src/main/Start.java
+++ src/main/Start.java
@@ -1,10 +1,24 @@
 package main;
 
+import java.util.List;
+
+import clonador.ClonadorPersona;
+import persona.Persona;
+
 public class Start {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		ClonadorPersona cp = new ClonadorPersona("Eduardo");
+		try {
+			List<Persona> personas = cp.generar();
+			System.out.println(personas);
+			
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		
 	}
 
 }
